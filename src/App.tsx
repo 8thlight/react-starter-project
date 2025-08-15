165201c1f6a79159cee899982fbd4202d3666cf0
Hugh Sato <hugh_sato@apple.com>
2025-08-15 12:07:54 -0500

    Setup react vite app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Product Page</h1>
+      <div className="card">
+        <p>
+          Placeholder Text
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
